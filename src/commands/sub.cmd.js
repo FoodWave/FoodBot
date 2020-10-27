4f9c7acdac151b4b310b385cefe418f86cd345c9
Antoine Rieuse <antoine.rieuse@epitech.eu>
2020-10-28 00:24:48 +0100

    init of sub.cmd

---
+++ src/commands/sub.cmd.js
@@ -0,0 +1,7 @@
+const name = "sub";
+const description = "Subscribe to our newsletter";
+const execute = (message, args) => {
+    console.log("sub command");
+};
+
+export { name, description, execute };
