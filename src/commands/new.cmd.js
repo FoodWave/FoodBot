6bd7f37a9f416ea1a59f514f35c6a108dfe977a4
Antoine Rieuse <antoine.rieuse@epitech.eu>
2020-10-28 17:17:54 +0100

    newsletter embed done

--- src/commands/new.cmd.js
+++ src/commands/new.cmd.js
@@ -1,11 +1,27 @@
+import Discord from "discord.js";
+
 const name = "new";
 const description =
   "For authorized users only: send a newsletter to subscribers";
 
-const new_command = (message, args) => {
+const send_newsletter = (message, args) => {
+  const newsletter_channel = message.guild.channels.cache.get(process.env.NEWSLETTER_CHANNEL_ID);
+  const newsletterEmbed = new Discord.MessageEmbed()
+    .setColor("#00FF00")
+    .setAuthor("Newsletter")
+    .setTitle(":warning: **INVENDUS DISPONIBLES** :warning:")
+    .setThumbnail(
+      "https://secure.gravatar.com/avatar/a46e25185d3da919b650a7003804fa5a"
+    )
+    .setFooter("FoodBot");
+
   if (args.length >= 2) {
-    message.channel.send(
-      `<@&${process.env.NEWSLETTER_ROLE_ID}>\n:warning: **INVENDUS DISPONIBLES** :warning:`
+    for (let i = 0; args[i]; i += 2) {
+      newsletterEmbed.addField(args[i], args[i + 1]);
+    }
+    newsletter_channel.send(newsletterEmbed);
+    newsletter_channel.send(
+      `<@&${process.env.NEWSLETTER_ROLE_ID}>`
     );
   } else {
     message.reply(
@@ -20,7 +36,7 @@ const execute = (message, args) => {
     message.member.roles.cache.has(process.env.MODERATOR_ROLE_ID)
   ) {
     if (message.channel.id === process.env.BOT_STUFF_ADMIN_CHANNEL_ID) {
-      new_command(message, args);
+      send_newsletter(message, args);
     } else {
       message.reply(
         `please send me this command in <#${process.env.BOT_STUFF_ADMIN_CHANNEL_ID}> \^\^`
