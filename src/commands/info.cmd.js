895bbb07740bf83e6022498d7cdc96c4c52e873a
Antoine Rieuse <antoine.rieuse@epitech.eu>
2020-10-28 19:18:06 +0100

    deleting info.cmd feature project

--- src/commands/info.cmd.js
+++
@@ -1,7 +0,0 @@
-const name = "info";
-const description = "To autorized users only: get infos about a user";
-const execute = (message, args) => {
-  console.log("info command");
-};
-
-export { name, description, execute };
