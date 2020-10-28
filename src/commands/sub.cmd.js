907b9afec30445e474d25d39cd31c0c4d1f542b6
Antoine Rieuse <antoine.rieuse@epitech.eu>
2020-10-28 01:50:21 +0100

    sub.cmd done

--- src/commands/sub.cmd.js
+++ src/commands/sub.cmd.js
@@ -1,7 +1,19 @@
+import { Guild } from "discord.js";
+
 const name = "sub";
 const description = "Subscribe to our newsletter";
-const execute = (message, args) => {
-    console.log("sub command");
+const execute = (message) => {
+  if (message.channel.id === process.env.BOT_STUFF_PUBLIC_CHANNEL_ID) {
+    if (!message.member.roles.cache.has(process.env.NEWSLETTER_ROLE_ID)) {
+      message.member.roles.add(process.env.NEWSLETTER_ROLE_ID);
+      message.reply(`you suscribed to our newsletter, thanks you ! :blush:`);
+    } else {
+        message.reply("you already suscribed to the newsletter :confused:");
+    }
+  } else {
+    message.reply(
+      `please send me your command in <#${process.env.BOT_STUFF_PUBLIC_CHANNEL_ID}> \^\^`
+    );
+  }
 };
-
 export { name, description, execute };
