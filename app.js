f6af875da4a3640bfd07aa00079227867d0f3191
Antoine Rieuse <antoine.rieuse@epitech.eu>
2020-10-28 02:54:06 +0100

    edit prefix info on start

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ const commands = commandFiles.map((file) =>
 // On bot login
 Bot.on("ready", () => {
   console.log(`Logged in as ${Bot.user.tag}`);
-  console.log(`Prefix is '${prefix}'`);
+  console.log(`Prefix set to '${prefix}'`);
 });
 
 // When bot detects a message
