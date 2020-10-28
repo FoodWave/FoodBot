9e1cdc8ae28fa487f9bc48cf131f2ca5b4013a2d
Antoine Rieuse <antoine.rieuse@epitech.eu>
2020-10-28 16:29:16 +0100

    info.cmd inial commit

---
+++ src/commands/info.cmd.js
@@ -0,0 +1,7 @@
+const name = "info";
+const description = "To autorized users only: get infos about a user";
+const execute = (message, args) => {
+  console.log("info command");
+};
+
+export { name, description, execute };
